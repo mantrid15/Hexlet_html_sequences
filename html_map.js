af142080284a8007fbbc2a1a278720f126ad56a0
mantrid15 <40058139+mantrid15@users.noreply.github.com>
2019-04-15 16:07:31 +0300

    Update html_map.js

--- html_map.js
+++ html_map.js
@@ -1,4 +1,48 @@
 /*
+import {
+	l, isEmpty, head, tail, cons, reverse,
+} from 'hexlet-pairs-data';
+
+import {
+	getName, getValue, node, is,
+} from 'hexlet-html-tags';
+
+import { reverse as reverseStr } from './strings';
+*/
+
+// BEGIN (write your solution here)
+export const map = (func, list) => {
+	const iter = (items, acc) => {
+		if (isEmpty(items)) {
+			return reverse(acc);
+		}
+		return iter(tail(items), cons(func(head(items)), acc));
+	};
+
+	return iter(list, l());
+};
+
+export const mirror = (html) => map(element => node(getName(element), reverseStr(getValue(element))), html);
+// END
+
+/*export const b2p = (elements) => {
+	if (isEmpty(elements)) {
+		return l();
+	}
+
+	let newElement;
+	const element = head(elements);
+	if (is('blockquote', element)) {
+		newElement = node('p', getValue(element));
+	} else {
+		newElement = element;
+	}
+
+	return cons(newElement, b2p(tail(elements)));
+};*/
+
+
+/*
 html-tags.js
 Реализуйте и экспортируйте функцию map для библиотеки html-tags. Реализация должна быть построена с использованием итеративного процесса (без циклов, на основе рекурсии). Эта функция подобна той что описывалась в теории для списков, только текущая реализация работает с html-списком. Параметры и их порядок у функций аналогичный. Первый — функция-трансформер, второй — коллекция (в нашем случае список html-тегов).
 
